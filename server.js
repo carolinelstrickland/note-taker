bb7842b790f6360905b710133a66dac6098cbdbe
Caroline Strickland <carolinelstrickland@outlook.com>
2020-09-19 17:49:36 -0500

    removing develop folder

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.get("/api/notes", function(req, res) {
 });
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.post("/api/notes", function(req, res){
